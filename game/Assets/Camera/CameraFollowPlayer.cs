4226285443613406b556267587f52007557d49c6
weiwei19890607 <weiwei19890607@hotmail.com>
2013-10-13 18:26:29 +0300

    Player movement, temporary player's view added

---
+++ game/Assets/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+	int DistanceAway = 5;
+	Vector3 PlayerPOS = GameObject.Find("Player").transform.transform.position;
+	
+	// Use this for initialization
+	void Start () {
+	transform.Rotate(30, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 PlayerPOS = GameObject.Find("Player").transform.transform.position;
+		transform.position = new Vector3(PlayerPOS.x, PlayerPOS.y + DistanceAway, PlayerPOS.z - DistanceAway);
+
+	}
+}
---
+++ game/Assets/Player/PlayerMovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+	public static int speed = 1;
+	float movement = 0.1f * speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		float player_x = Input.GetAxis("Mouse X");
+//		float player_y = Input.GetAxis("Mouse Y");
+		
+//		transform.position = new Vector3(transform.position.x + player_x, transform.position.y + player_y, 0);
+	
+		if (Input.GetKey (KeyCode.UpArrow)){
+			rigidbody.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + movement);	
+		
+		}
+		else if (Input.GetKey (KeyCode.DownArrow)){
+			rigidbody.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - movement);
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow)){
+			rigidbody.position = new Vector3 (transform.position.x - movement, transform.position.y, transform.position.z);
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)){
+			rigidbody.position = new Vector3 (transform.position.x + movement, transform.position.y, transform.position.z);
+		}
+		
+	}
+}
