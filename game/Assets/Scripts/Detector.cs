45db02ed4af08958e8f496bab778c1de48a0f085
Tony Kovanen <tonykovanen@hotmail.com>
2013-10-18 15:40:13 +0300

    Let's herpderp this

--- game/Assets/Scripts/Detector.cs
+++ game/Assets/Scripts/Detector.cs
@@ -1,50 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Detector : MonoBehaviour {
+public class Detector : MonoBehaviour
+{
 
-	private GUIStyle menuStyle;
+	public GUIStyle menuStyle;
 	public Transform target; //checks if the player is visible and nothing is blocking the view
 	
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
-	void Update (){
-		if(CanSeePlayer()){
-			Reset ();
+	void Update ()
+	{
+		if (CanSeePlayer ()) {
+			WaitAndLoadLevel(2.0f);
 		}
 	}
 	
-	void Reset(){
-		OnGUI();
-		Application.LoadLevel(Application.loadedLevel);
-	}
-	
-	bool CanSeePlayer(){
-		Vector3 viewPos = GameObject.FindGameObjectWithTag("activeCam").GetComponent<Camera>().WorldToViewportPoint (target.position);
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		Vector3 here = GameObject.FindGameObjectWithTag("activeCam").transform.position;
-		Vector3 pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+	bool CanSeePlayer ()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		Vector3 viewPos = GameObject.FindGameObjectWithTag ("activeCam").GetComponent<Camera> ().WorldToViewportPoint (player.transform.position);
+		Vector3 here = GameObject.FindGameObjectWithTag ("activeCam").transform.position;
+		Vector3 pos = GameObject.FindGameObjectWithTag ("Player").transform.position;
 		// do a Linecast:
-      	RaycastHit hit; 
-		Physics.Linecast(here, pos, out hit);
-		if(checkViewPos(viewPos) && hit.transform == player.transform){
-			return false;
-		}
-		return true;
-	}
-	
-	bool checkViewPos(Vector3 viewPos){
-		if(viewPos.x>0 && viewPos.y>0 && viewPos.x<1 && viewPos.y<1 && viewPos.z>0){
+		RaycastHit hit; 
+		bool linecastHit = Physics.Raycast (here, pos, out hit);
+		//if (linecastHit && checkViewPos (viewPos) && hit.transform == player.transform) {
+		if (player.collider == hit.collider) Debug.Log("HMMMMMMMMMMMMM collider mätsää");
+		if (linecastHit) Debug.Log ("Hmm linecast hittaa");
+		if (checkViewPos (viewPos)) Debug.Log ("Posseee");
+		Debug.DrawLine(player.transform.position, player.transform.position + new Vector3(10,10,0));
+		Debug.DrawLine(target.transform.position, target.transform.position + new Vector3(10,10,0));
+		if (linecastHit && checkViewPos (viewPos) && hit.collider == player.collider) {
+			Debug.LogWarning(" NO VITTU KOKO AJAN TRUE SAATANA ");
 			return true;
 		}
-		else{
+		return false;
+	}
+	
+	bool checkViewPos (Vector3 viewPos)
+	{
+		if (viewPos.x > 0 && viewPos.y > 0 && viewPos.x < 1 && viewPos.y < 1 && viewPos.z > 0) {
+			return true;
+		} else {
 			return false;
 		}
 	}
 	
-	void OnGUI(){
-		GUI.Label(new Rect(Screen.width/2-200,Screen.height/2-100,1000,50),"Camera spotted you!", menuStyle);
+	void LoadGui ()
+	{
+		GUI.Label (new Rect (Screen.width / 2 - 200, Screen.height / 2 - 100, 1000, 50), "Camera spotted you!", menuStyle);
+	}
+	
+	IEnumerator WaitAndLoadLevel (float wait)
+	{
+		Debug.LogError("NO VOI HELVETTI");
+		LoadGui ();
+		yield return new WaitForSeconds(wait);
+		Application.LoadLevel (Application.loadedLevel);
 	}
 }
