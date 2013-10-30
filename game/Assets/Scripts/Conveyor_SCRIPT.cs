ba552fafd1ea121eba48bbc638076e84e396ea3d
Misacorp <misa.jokisalo@gmail.com>
2013-10-30 14:57:40 +0200

    Conveyor belts and jump pads
    Added conveyor belt
    - Moves all objects with a rigidbody in conveyor toward conveyor's Z
    axis
    - Currently the player cannot walk on the conveyor for additional speed
    
    Added jump pad
    - Bounces the player upwards

---
+++ game/Assets/Scripts/Conveyor_SCRIPT.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conveyor_SCRIPT : MonoBehaviour {
+	
+	public float speed = 0.5f;
+	public float maxSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//	Moves player (can be set to any object with tags) in direction of CONVEYOR BELT'S LOCAL Z AXIS
+	void OnCollisionStay(Collision col)	
+	{
+		if(col.gameObject.rigidbody == null)	{
+			return;
+		}
+
+		Rigidbody rigidbody = col.gameObject.rigidbody;
+		
+		if(rigidbody.velocity.magnitude < maxSpeed)
+			rigidbody.AddForce(speed * transform.forward, ForceMode.VelocityChange);
+	}
+}
