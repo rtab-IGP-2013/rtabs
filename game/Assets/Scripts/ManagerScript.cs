4c4a01b3ca31affbe7cdeff99528eea6677bfe7e
Tony Kovanen <tonykovanen@hotmail.com>
2013-11-04 16:19:32 +0200

    Deleted Manager_SCRIPT.js and replaced with a C# script file

---
+++ game/Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerScript : MonoBehaviour {
+	public GameObject player;
+	public Transform startMarker;
+	
+	// Use this for initialization
+	void Start () {
+		Instantiate(player, startMarker.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
