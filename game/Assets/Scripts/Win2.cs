c527dc117ce9ef346cedbf87675f85b15a463b9c
Vaakapallo <lassi.vapaakallio@cs.helsinki.fi>
2013-11-06 17:44:29 +0200

    menufixes and stuff

--- game/Assets/Scripts/Win2.cs
+++ game/Assets/Scripts/Win2.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Win2 : MonoBehaviour {
+public class Win2 : MonoBehaviour
+{
 	static string text = "";
-	public static bool WinOrNot = false;
-	void OnGUI(){
-		GUI.Label(new Rect(10,10,200,80),text);
+
+	void OnGUI ()
+	{
+		GUI.Label (new Rect (10, 10, 200, 80), text);
 	}
-		IEnumerator OnTriggerEnter (Collider box) {
+
+	IEnumerator OnTriggerEnter (Collider box)
+	{
 
 		text = "You win the game!!!";
-		WinOrNot = true;
+		WinCondition.WinOrNot = true;
 		yield return new WaitForSeconds(2);
-		Application.LoadLevel ("turningCameras"); 
-
-}
+		WinCondition.WinOrNot = false;
+		text = "";
+		Application.LoadLevel ("menu"); 
+	}
 
 }
 
