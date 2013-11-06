ee0fa01ecc47b9cc4cc4a69ea53368759e4cc196
Mikko Tamminen <pahanaama@gmail.com>
2013-11-06 18:35:56 +0200

    Remade player, detector now works with the corners of player

--- game/Assets/Scripts/Detector.cs
+++ game/Assets/Scripts/Detector.cs
@@ -5,41 +5,41 @@ public class Detector : MonoBehaviour
 {
 
 	public GUIStyle menuStyle;
-	public Transform target; 		//checks if the player is visible and nothing is blocking the view
 	public Vector3 threshold;		//	How fast the player can move without being detected. Use this to compensate for movement smoothing (deceleration) if even necessary.
-	public Vector3 start_position;
 	public Transform start_marker;
 	GameObject player;
 	private SuspicionMeter suspicionMeter;
-	Transform playerObject;
+	public GUIText debugGui;
+	private static float dToCorner = 0.4f;
+	private Vector3[] corners = new Vector3[] {
+			new Vector3 (dToCorner, dToCorner, dToCorner),
+			new Vector3 (dToCorner, dToCorner, -dToCorner),
+			new Vector3 (dToCorner, -dToCorner, dToCorner),
+			new Vector3 (dToCorner, -dToCorner, -dToCorner),
+			
+			new Vector3 (-dToCorner, dToCorner, dToCorner),
+			new Vector3 (-dToCorner, dToCorner, -dToCorner),
+			new Vector3 (-dToCorner, -dToCorner, dToCorner),
+			new Vector3 (-dToCorner, -dToCorner, -dToCorner)
+		};
 	
 	void Start ()
 	{
-		start_position = start_marker.position; //setting the respawn point
+		player = GameObject.FindGameObjectWithTag ("Player");
 		threshold = new Vector3 (0.8f, 0, 0);		//	The direction of this vector does not matter, only the magnitude.
-		FindPlayer();
-		playerObject = player.transform.Find ("w_box_5_w_box_5_w_box_5");
 	}
 	
 	void Update ()
 	{
-		if (player == null) FindPlayer ();
-		
+		if (player == null) {
+			player = GameObject.FindGameObjectWithTag ("Player");	
+		}
 		if (CanSeePlayer ()) {
-			Debug.Log ("Seen player");
 			if (playerMoving () && !WinCondition.WinOrNot) {
 				Debug.Log ("Seen player moving");
-				this.gameObject.SendMessage("AdjustSuspicionBar",3 , SendMessageOptions.RequireReceiver);
-				//player.transform.position = start_position;  //port player to respawn BROKEN
-						
-				// WaitAndLoadLevel(2.0f);
+				this.gameObject.SendMessage ("AdjustSuspicionBar", 3, SendMessageOptions.RequireReceiver);
 			}
 		}
-	}
-	
-	void FindPlayer() {
-		player = GameObject.FindGameObjectWithTag ("Player");
-		playerObject = player.transform.Find ("w_box_5_w_box_5_w_box_5");
 	}
 	
 	//	Checks if the active camera sees the player. If the player is behind another object the player is not seen.
@@ -50,11 +50,18 @@ public class Detector : MonoBehaviour
 		Vector3 viewPos = CameraManger.getActiveCamera ().WorldToViewportPoint (player.transform.position);
 		Vector3 here = CameraManger.getActiveCamera ().transform.position;
 		Vector3 pos = player.transform.position;
-		RaycastHit hit;
 		
-		bool linecastHit = Physics.Linecast (here, pos, out hit);
-		if (linecastHit && checkViewPos (viewPos) && (playerObject.collider == hit.collider)) {
-			return true;
+		foreach (Vector3 v in corners) {
+			RaycastHit rch;
+			bool didhit = Physics.Linecast (here, pos + v, out rch);
+			
+			if (checkViewPos (viewPos) && didhit) {
+				if (rch.transform == player.transform) {
+					Debug.Log("You are seen");
+					return true;
+				}
+//				Debug.DrawRay (new Vector3 (0, 0, 0), rch.transform.position);
+			}
 		}
 		return false;
 	}
@@ -75,18 +82,5 @@ public class Detector : MonoBehaviour
 			return true;
 		else
 			return false;
-	}
-	
-	void LoadGui ()
-	{
-		GUI.Label (new Rect (Screen.width / 2 - 200, Screen.height / 2 - 100, 1000, 50), "Camera spotted you!", menuStyle);
-	}
-	
-//	IEnumerator WaitAndLoadLevel (float wait)
-	void WaitAndLoadLevel (float wait)
-	{
-		LoadGui ();
-		//yield return new WaitForSeconds(wait);
-		Application.LoadLevel (Application.loadedLevel);
 	}
 }
