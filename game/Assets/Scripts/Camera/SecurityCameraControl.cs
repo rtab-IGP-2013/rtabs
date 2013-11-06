bcc7287cde0c0f2c44a3e61be1b4c7bab7b30c2a
Vaakapallo <lassi.vapaakallio@cs.helsinki.fi>
2013-11-06 17:13:49 +0200

    retouched the first level to be cooler

--- game/Assets/Scripts/Camera/SecurityCameraControl.cs
+++ game/Assets/Scripts/Camera/SecurityCameraControl.cs
@@ -1,44 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SecurityCameraControl : MonoBehaviour {
-	public enum Axis { HORIZONTAL, VERTICAL };
+public class SecurityCameraControl : MonoBehaviour
+{
+	public enum Axis
+	{
+		HORIZONTAL,
+		VERTICAL
+	};
 	
 	public Axis axis = Axis.HORIZONTAL;
 	public int positiveRadius = 200;
 	public int negativeRadius = 500;
 	public float turningSpeed = 0.8f;
-	
 	float currentAngle;
 	public bool movingInPositiveDirection = true;
 	
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		currentAngle = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (axis == Axis.VERTICAL) {
 			if (movingInPositiveDirection) {
-				transform.Rotate (new Vector3(turningSpeed, 0, 0));
+				transform.Rotate (new Vector3 (turningSpeed, 0, 0));
 				currentAngle++;
-				if (currentAngle >= positiveRadius) movingInPositiveDirection = false; 
+				if (currentAngle >= positiveRadius)
+					movingInPositiveDirection = false; 
 			} else {
-				transform.Rotate (new Vector3(-turningSpeed, 0, 0));
+				transform.Rotate (new Vector3 (-turningSpeed, 0, 0));
 				currentAngle--;
-				if (currentAngle <= -negativeRadius) movingInPositiveDirection = true;
+				if (currentAngle <= -negativeRadius)
+					movingInPositiveDirection = true;
 			}
 		} else {
 			if (movingInPositiveDirection) {
-				transform.Rotate (new Vector3(0, turningSpeed, 0));
+				transform.Rotate (new Vector3 (0, turningSpeed, 0));
 				currentAngle++;
-				if (currentAngle >= positiveRadius) movingInPositiveDirection = false; 
+				if (currentAngle >= positiveRadius)
+					movingInPositiveDirection = false; 
 			} else {
-				transform.Rotate (new Vector3(0, -turningSpeed, 0));
+				transform.Rotate (new Vector3 (0, -turningSpeed, 0));
 				currentAngle--;
-				if (currentAngle <= -negativeRadius) movingInPositiveDirection = true;
+				if (currentAngle <= -negativeRadius)
+					movingInPositiveDirection = true;
 			}
 		}
 	}
