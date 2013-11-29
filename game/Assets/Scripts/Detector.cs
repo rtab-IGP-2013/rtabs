0d50109a9431aeba1338a318fa9fa2631e1484bd
villvirt <ville@mevirtaset.com>
2013-11-29 02:20:52 +0200

    stairs level, still needs finishing touch

--- game/Assets/Scripts/Detector.cs
+++ game/Assets/Scripts/Detector.cs
@@ -4,18 +4,14 @@ using System.Collections;
 public class Detector : MonoBehaviour
 {
 
-	public GUIStyle menuStyle;
-	public Transform target; 		//  Checks if the player is visible and nothing is blocking the view
-	public Vector3 threshold;		//	How fast the player can move without being detected. Use this to compensate for movement smoothing (deceleration) if even necessary.
-	private GameObject player;
-	private SuspicionMeter suspicionMeter;
-	private int decreaseCounter;
-	private int increaseCounter;
-	private int increaseSuspicion=15; //increase every 4 frames
-	private int decreaseSuspicion=5; // decrease every 5 frames
-	private Transform playerObject;
-	private static float dToCorner = 0.4f;
-	private Vector3[] corners = new Vector3[] {
+        public GUIStyle menuStyle;
+        public Transform target;                 //  Checks if the player is visible and nothing is blocking the view
+        public Vector3 threshold;                //        How fast the player can move without being detected. Use this to compensate for movement smoothing (deceleration) if even necessary.
+        private GameObject player;
+        private SuspicionMeter suspicionMeter;
+        private Transform playerObject;
+        private static float dToCorner = 0.4f;
+        private Vector3[] corners = new Vector3[] {
                         new Vector3 (dToCorner, dToCorner, dToCorner),
                         new Vector3 (dToCorner, dToCorner, -dToCorner),
                         new Vector3 (dToCorner, -dToCorner, dToCorner),
@@ -26,89 +22,91 @@ public class Detector : MonoBehaviour
                         new Vector3 (-dToCorner, -dToCorner, dToCorner),
                         new Vector3 (-dToCorner, -dToCorner, -dToCorner)
                 };
-	
-	void Start ()
-	{
-        threshold = new Vector3(0.8f, 0, 0);		//	The direction of this vector does not matter, only the magnitude.
+        
+        void Start ()
+        {
+        threshold = new Vector3(0.8f, 0, 0);                //        The direction of this vector does not matter, only the magnitude.
         // player = GameObject.FindGameObjectWithTag("Player");
         // playerObject = GameObject.FindGameObjectWithTag("Player").transform;
-		FindPlayer ();
-	}
-	
-	void Update ()
-	{
-		if (player == null){
-			FindPlayer ();
-		}
-		if (CanSeePlayer ()) {
-			if(increaseCounter==0){
-			this.gameObject.SendMessage ("AdjustSuspicionBar", 1, SendMessageOptions.RequireReceiver);
-			// Debug.Log ("Seen player");
-			}
-			if (playerMoving () && !WinCondition.WinOrNot) {
-				// Debug.Log ("Seen player moving");
-				this.gameObject.SendMessage ("AdjustSuspicionBar", 5, SendMessageOptions.RequireReceiver);
-						
-				// WaitAndLoadLevel(2.0f);
-			}
-		}
-		else if(decreaseCounter==0 && !CanSeePlayer ()){
-			this.gameObject.SendMessage ("AdjustSuspicionBar", -1, SendMessageOptions.RequireReceiver);
-			
-		}
-		increaseCounter = (increaseCounter + 1) % increaseSuspicion;
-		decreaseCounter = (decreaseCounter + 1) % decreaseSuspicion;
-	}
-	
-	void FindPlayer ()
-	{
-		player = GameObject.FindGameObjectWithTag ("Player");
+                FindPlayer ();
+        }
+        
+        void Update ()
+        {
+                if (player == null){
+                        FindPlayer ();
+                }
+                if (CanSeePlayer ()) {
+                        // Debug.Log ("Seen player");
+                        if (playerMoving () && !WinCondition.WinOrNot) {
+                                // Debug.Log ("Seen player moving");
+                                this.gameObject.SendMessage ("AdjustSuspicionBar", 3, SendMessageOptions.RequireReceiver);
+                                                
+                                // WaitAndLoadLevel(2.0f);
+                        }
+                }
+        }
+        
+        void FindPlayer ()
+        {
+                player = GameObject.FindGameObjectWithTag ("Player");
         if (player != null) {                                       //  This is thrown by the game only once at the very start. Running a check to avoid an exception! :-D
             playerObject = player.transform;
         }
-	}
-	
-	//	Checks if the active camera sees the player. If the player is behind another object the player is not seen.
-	//	TODO:	Only detects if the center of the player object is visible. Needs to be changed to the whole player collider.
-	//			player.collider.bounds.Contains (hit.transform.position) doesn't seem to be doing the trick :(
-	bool CanSeePlayer ()
-	{
-		Vector3 viewPos = CameraManager.getActiveCamera ().WorldToViewportPoint (player.transform.position);
-		Vector3 here = CameraManager.getActiveCamera ().transform.position;
-		Vector3 pos = player.transform.position;
+        }
+        
+        //        Checks if the active camera sees the player. If the player is behind another object the player is not seen.
+        //        TODO:        Only detects if the center of the player object is visible. Needs to be changed to the whole player collider.
+        //                        player.collider.bounds.Contains (hit.transform.position) doesn't seem to be doing the trick :(
+        bool CanSeePlayer ()
+        {
+                Vector3 viewPos = CameraManager.getActiveCamera ().WorldToViewportPoint (player.transform.position);
+                Vector3 here = CameraManager.getActiveCamera ().transform.position;
+                Vector3 pos = player.transform.position;
                 
-		foreach (Vector3 v in corners) {
-			RaycastHit rch;
-			bool didhit = Physics.Linecast (here, pos + v, out rch);
-			Debug.DrawRay (new Vector3 (0, 0, 0), pos + v);
-			if (checkViewPos (viewPos) && didhit) {
-				if (rch.transform == player.transform) {
-					// Debug.Log ("You are seen");
-					return true;
-				}
+                foreach (Vector3 v in corners) {
+                        RaycastHit rch;
+                        bool didhit = Physics.Linecast (here, pos + v, out rch);
+                        Debug.DrawRay (new Vector3 (0, 0, 0), pos + v);
+                        if (checkViewPos (viewPos) && didhit) {
+                                if (rch.transform == player.transform) {
+                                        // Debug.Log ("You are seen");
+                                        return true;
+                                }
 //                                Debug.DrawRay (new Vector3 (0, 0, 0), rch.transform.position);
-			}
-		}
-		return false;
-	}
-	
-	//	Checks if player is within the given view.
-	bool checkViewPos (Vector3 viewPos)
-	{
-		if (viewPos.x > 0 && viewPos.y > 0 && viewPos.x < 1 && viewPos.y < 1 && viewPos.z > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	bool playerMoving ()
-	{	
-		if (player.rigidbody.velocity.magnitude > threshold.magnitude)
-			return true;
-		else
-			return false;
-	}
-	
-
+                        }
+                }
+                return false;
+        }
+        
+        //        Checks if player is within the given view.
+        bool checkViewPos (Vector3 viewPos)
+        {
+                if (viewPos.x > 0 && viewPos.y > 0 && viewPos.x < 1 && viewPos.y < 1 && viewPos.z > 0) {
+                        return true;
+                } else {
+                        return false;
+                }
+        }
+        
+        bool playerMoving ()
+        {        
+                if (player.rigidbody.velocity.magnitude > threshold.magnitude)
+                        return true;
+                else
+                        return false;
+        }
+        
+        void LoadGui ()
+        {
+                GUI.Label (new Rect (Screen.width / 2 - 200, Screen.height / 2 - 100, 1000, 50), "Camera spotted you!", menuStyle);
+        }
+        
+//        IEnumerator WaitAndLoadLevel (float wait)
+        void WaitAndLoadLevel (float wait)
+        {
+                LoadGui ();
+                //yield return new WaitForSeconds(wait);
+                Application.LoadLevel (Application.loadedLevel);
+        }
 }
--- game/Assets/Scripts/SuspicionMeter.cs
+++ game/Assets/Scripts/SuspicionMeter.cs
@@ -3,50 +3,50 @@ using System.Collections;
 
 
 public class SuspicionMeter : MonoBehaviour {
-	private int maxSuspicion = 100;
-	private int currentSuspicion = 0;
-	private float suspicionBarLength;
-	private int frameCounter = 0;
-	private int decaysSuspicion = 5; // every 5 frames
+        private int maxSuspicion = 100;
+        private int currentSuspicion = 0;
+        private float suspicionBarLength;
+        private int frameCounter = 0;
+        private int decaysSuspicion = 5; // every 5 frames
     private string text = "SUSPICIOUS";
-	
-	// Use this for initialization
-	void Start () {
-		suspicionBarLength = Screen.width / 2;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (this.currentSuspicion == maxSuspicion) {
-			currentSuspicion = 0;
-			this.gameObject.SendMessage ("KillPlayer", SendMessageOptions.RequireReceiver);
-		}
-		
-		/*if (frameCounter == 0) {
-			AdjustSuspicionBar(-1);
-		}
-		frameCounter = (frameCounter + 1) % decaysSuspicion;*/
-	}
-	
-	void OnGUI() {
+        
+        // Use this for initialization
+        void Start () {
+                suspicionBarLength = Screen.width / 2;
+        }
+        
+        // Update is called once per frame
+        void Update () {
+                if (this.currentSuspicion == maxSuspicion) {
+                        currentSuspicion = 0;
+                        this.gameObject.SendMessage ("KillPlayer", SendMessageOptions.RequireReceiver);
+                }
+                
+                if (frameCounter == 0) {
+                        AdjustSuspicionBar(-1);
+                }
+                frameCounter = (frameCounter + 1) % decaysSuspicion;
+        }
+        
+        void OnGUI() {
 
         GUI.Box(new Rect(Screen.width / 2 - 200, 10, Mathf.Max(suspicionBarLength, 15), 20), text.Substring(0, Mathf.Min((int)Mathf.Floor(currentSuspicion / (text.Length-2)), text.Length))); // As numbers: currentSuspicion + "/" + maxSuspicion
-	}
-	
-	void ResetSuspicionMeter() {
-		currentSuspicion = 0;	
-	}
-	
-	void AdjustSuspicionBar(int adj) {
-		currentSuspicion += adj;
-		if (currentSuspicion < 0) {
-			currentSuspicion = 0;
-		}
-		
-		if (currentSuspicion > maxSuspicion) {
-			currentSuspicion = maxSuspicion;	
-		}
-		
-		 suspicionBarLength = (Screen.width / 2) * (currentSuspicion / (float)maxSuspicion);
-	}
+        }
+        
+        void ResetSuspicionMeter() {
+                currentSuspicion = 0;        
+        }
+        
+        void AdjustSuspicionBar(int adj) {
+                currentSuspicion += adj;
+                if (currentSuspicion < 0) {
+                        currentSuspicion = 0;
+                }
+                
+                if (currentSuspicion > maxSuspicion) {
+                        currentSuspicion = maxSuspicion;        
+                }
+                
+                 suspicionBarLength = (Screen.width / 2) * (currentSuspicion / (float)maxSuspicion);
+        }
 }
