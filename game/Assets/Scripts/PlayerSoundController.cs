4f1a250e31aa96b696c22ae9834ef418cd8c908f
Misacorp <misa.jokisalo@gmail.com>
2013-12-02 02:19:40 +0200

    Sound effects
    - Implemented sound effect for moving
    - Other SFX are in, but not implemented

---
+++ game/Assets/Scripts/PlayerSoundController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSoundController : MonoBehaviour {
+
+	public AudioClip slideSound;
+	public AudioClip dropSound;
+
+	// Use this for initialization
+	void Start () {
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.tag == "floor") {
+
+		}
+	}
+
+	void OnCollisionStay(Collision col)	{
+		if(col.gameObject.tag == "floor") {
+			audio.volume = gameObject.transform.rigidbody.velocity.magnitude / gameObject.GetComponent<PlayerMovementController>().maxspeed;
+		}
+	}
+
+	void OnCollisionExit(Collision col)	{
+		if(col.gameObject.tag == "floor") {
+			audio.volume = 0;
+			Debug.Log("Exited collision");
+		}
+	}
+
+}
