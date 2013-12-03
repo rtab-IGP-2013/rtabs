ecf6b8b8d2c7e46df0eff2f3f12555588abd1fa4
Tony Kovanen <tonykovanen@hotmail.com>
2013-12-03 17:10:02 +0200

    Ditched the progress bar and we now have a black screen of death

--- game/Assets/Scripts/LevelLoaderScript.cs
+++ game/Assets/Scripts/LevelLoaderScript.cs
@@ -3,22 +3,21 @@ using System.Collections;
 
 public class LevelLoaderScript : MonoBehaviour
 {
-	float progress = 0.0f;
-	Vector2 pos = new Vector2 ((Screen.width / 2) - 300, (Screen.height / 2) - 10);
-	Vector2 size = new Vector2 (600, 20);
-	public Texture2D progressBarEmpty;
-	public Texture2D progressBarFull;
+//	float progress = 0.0f;
+//	Vector2 pos = new Vector2 ((Screen.width / 2) - 300, (Screen.height / 2) - 10);
+//	Vector2 size = new Vector2 (600, 20);
+//	public Texture2D progressBarEmpty;
+//	public Texture2D progressBarFull;
 	
 	// Use this for initialization
 	void Start ()
 	{
-	
 	}
 	
 	void OnGUI ()
 	{
-		GUI.DrawTexture (new Rect (pos.x, pos.y, size.x, size.y), progressBarEmpty);
-		GUI.DrawTexture (new Rect (pos.x, pos.y, size.x * Mathf.Clamp01 (progress), size.y), progressBarFull);
+		//GUI.DrawTexture (new Rect (pos.x, pos.y, size.x, size.y), progressBarEmpty);
+		//GUI.DrawTexture (new Rect (pos.x, pos.y, size.x * Mathf.Clamp01 (progress), size.y), progressBarFull);
 	} 
 	
 	// Update is called once per frame
@@ -27,7 +26,6 @@ public class LevelLoaderScript : MonoBehaviour
 		if (Application.GetStreamProgressForLevel (Application.loadedLevel + 1) == 1) {
 			Application.LoadLevel (Application.loadedLevel + 1);
 		}
-		progress = Application.GetStreamProgressForLevel(Application.loadedLevel + 1);
-		
+		//progress = Application.GetStreamProgressForLevel(Application.loadedLevel + 1);
 	}
 }
