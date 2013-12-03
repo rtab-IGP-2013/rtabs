844dbac39ba8afc2c2234c2c056ffb0c4a840664
weiwei19890607 <weiwei19890607@hotmail.com>
2013-12-03 23:49:31 +0800

    Menu improvement and Credits added

---
+++ game/Assets/Scripts/EndListController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndListController : MonoBehaviour
+{
+	private float scrollspeed;
+	private string list = "              RTABS\n\n\n         -Programming-\n\n            Gameplay\n\n        Lassi Vapaakallio\n\n          Misa Jokisalo\n\n          Ville Virtanen\n\n         Tony Kovanen\n\n            Jiawei Jin\n\n\n               Sound\n\n          Misa Jokisalo\n\n          Ville Virtanen\n\n         Tony Kovanen\n\n            Jiawei Jin\n\n       Lassi Vapaakallio\n\n\n              Physics\n\n          Ville Virtanen\n\n         Tony Kovanen\n\n            Jiawei Jin\n\n       Lassi Vapaakallio\n\n          Misa Jokisalo\n\n\n          -Game Design-\n\n         Tony Kovanen\n\n            Jiawei Jin\n\n       Lassi Vapaakallio\n\n          Misa Jokisalo\n\n          Ville Virtanen\n\n\n          -Level Design-\n\n            Jiawei Jin\n\n       Lassi Vapaakallio\n\n          Misa Jokisalo\n\n          Ville Virtanen\n\n         Tony Kovanen\n\n\n        -Concept Design-\n\n        Lassi Vapaakallio\n\n\n     -Original Soundtrack-\n\n           Jimi Welling\n\n\n      -Sound Engineering-\n\n          Juuso Leinonen\n\n           Misa Jokisalo\n\n\n           -Voice Actor-\n\n           Misa Jokisalo\n\n\n-Animation Technical Director-\n\n         Misacorp Visuals\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n   Thank you for playing!";
+	private bool switchToMenu;
+	private int i;
+	private float flagTime;
+	public GUIStyle myStyle;
+	// Use this for initialization
+	void Start ()
+	{
+		
+		scrollspeed = 0;
+		i = 0;
+		flagTime  = 0f;
+	}
+	
+	void OnGUI ()
+	{
+		
+		GUI.Label (new Rect (Screen.width / 2-100, Screen.height - scrollspeed, 400, 4000), list,myStyle);
+		if(scrollspeed <= 3990){
+			scrollspeed = scrollspeed + (float)Screen.height * 0.001f;
+		}
+		
+		if(!switchToMenu && scrollspeed > 3990){
+			switchToMenu = !switchToMenu;
+			
+			
+		}
+		
+		
+			
+		
+	}
+	
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(switchToMenu){
+			flagTime += Time.deltaTime;
+		}
+		if (flagTime > 4){
+			Application.LoadLevel("menu");
+		}
+		
+	}
+}
