c19d4dbcde332309ba972507e10864be702a05af
Lassi Vapaakallio <Lassi.Vapaakallio@cs.helsinki.fi>
2013-12-08 17:20:21 +0200

    fixing triggerbuttons in levels 7 and 0

---
+++ game/Assets/Scripts/SwitchOnScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchOnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(){
+		this.renderer.material.mainTextureOffset = new Vector2(0,0);	
+	}
+}
