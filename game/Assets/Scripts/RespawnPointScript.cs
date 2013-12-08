4055cb682828c8184ada942a6150d32049116159
Lassi Vapaakallio <Lassi.Vapaakallio@cs.helsinki.fi>
2013-12-08 17:08:35 +0200

    Respawn points for tutorial

---
+++ game/Assets/Scripts/RespawnPointScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnPointScript : MonoBehaviour {
+	
+	public Transform startMarker;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(){
+		startMarker.position = this.transform.position;	
+	}
+}
